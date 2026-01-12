57f9297b823de6bfd4e9ea8aff58f3ead46375ce
KimSummerrain <doeun457@gmail.com>
2026-01-12 14:56:06 +0900

    [update] .gitignore 수정

--- src/main/java/com/example/lionproject2backend/global/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/lionproject2backend/global/exception/GlobalExceptionHandler.java
@@ -8,6 +8,7 @@ import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 import com.example.lionproject2backend.global.exception.custom.CustomException;
 import com.example.lionproject2backend.global.exception.custom.ErrorCode;
